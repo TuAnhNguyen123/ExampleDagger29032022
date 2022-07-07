bb32ef51b13e1bf6e9433e893fb2603adb7585c7
JoseNguyen <abc@example.com>
2022-07-07 19:27:11 +0700

    set up package

--- app/src/main/java/com/example/appsales29032022/Banhxe.java
+++
@@ -1,7 +0,0 @@
-package com.example.appsales29032022;
-
-/**
- * Created by ntudroid on 7/7/2022.
- */
-public class Banhxe {
-}
---
+++ app/src/main/java/com/example/appsales29032022/presentation/view/activity/sign_in/SignInActivity.java
@@ -0,0 +1,17 @@
+package com.example.appsales29032022.presentation.view.activity.sign_in;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appsales29032022.R;
+
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
